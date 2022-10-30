2faec97bdc0fc526235005f6bec5c08144f111c0
ikramerekabi <78406187+ikramerekabi@users.noreply.github.com>
2022-10-30 20:16:07 +0200

    Add files via upload
    This is the detect_fine function

---
+++ detect_fine.cpp
@@ -0,0 +1,11 @@
+Bool detect_fine( double maximum_carbon_emission, double car_carbon_emission)
+{
+# if statement to check car carbon's emission with the allowed maximum by the government 
+ if (car_carbon_emission > maximum_carbon_emission)
+Return true
+# return true to state there is a fine on the car
+Else
+Return false
+# return false to state there is no fine
+
+}
