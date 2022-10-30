4e1f1abe88731df2ab309bd703f7979463d116c0
ikramerekabi <78406187+ikramerekabi@users.noreply.github.com>
2022-10-30 20:11:46 +0200

    Add files via upload
    Here is the login pseudo function

---
+++ login.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+void login(string car_plate)
+{
+    bool is_in_database; 
+    bool there_is_fine; 
+    float fine; 
+    //check if the car_plate is in the database 
+    if(is_in_database )
+    {
+          cout<<"Your carbon emission level is: "<<endl ;
+        if(there_is_fine)
+        {
+            // get fine amount from the database tagged to the car plate and display it on the user account. 
+            cout<<"The fine is"<<fine <<endl;
+        }
+        
+    }
+    
+    
+
+}
